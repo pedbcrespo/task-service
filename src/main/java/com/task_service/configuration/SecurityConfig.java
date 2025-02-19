8bf7fad291ad3adab3449e871608ebfd210d1488
pedro <pedbcrespo@gmail.com>
2025-02-18 21:25:01 -0300

    implementando seguranca

--- src/main/java/com/task_service/configuration/SecurityConfig.java
+++ src/main/java/com/task_service/configuration/SecurityConfig.java
@@ -2,8 +2,13 @@ package com.task_service.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,7 +22,18 @@ public class SecurityConfig {
                 .requestMatchers("/**").permitAll()
                 .anyRequest().authenticated()
             )
+            .httpBasic(withDefaults())
 			.csrf(csrf -> csrf.disable());
         return http.build();
+    }
+
+    @Bean
+    protected AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    protected PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
