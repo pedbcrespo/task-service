38ff47e22510b08750c149af6c85e0bb1a1747d4
pedro <pedbcrespo@gmail.com>
2025-06-07 11:37:28 -0300

    criando mapa com possibilidade de apontar local

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <map-location/>
+</template>
+<script>
+import MapLocation from './MapLocation.vue';
+export default {
+    components: {MapLocation},
+    data() {
+        return {}
+    }
+}
+</script>
---
+++ src/components/MapLocation.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="map-location">
+        <l-map
+          style="height: 400px;"
+          :zoom="13"
+          :center="[lat, lng]"
+          @click="onMapClick"
+        >
+          <l-tile-layer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <l-marker v-if="markerLat !== null" :lat-lng="[markerLat, markerLng]" />
+        </l-map>
+      
+        <p v-if="markerLat !== null">
+          Local clicado: {{ markerLat }}, {{ markerLng }}
+        </p>
+    </div>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker } from '@vue-leaflet/vue-leaflet'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png'
+})
+export default {
+  name: 'MapaComponent',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+  data() {
+    return {
+      lat: -22.9,
+      lng: -43.2,
+      markerLat: null,
+      markerLng: null
+    }
+  },
+  methods: {
+    onMapClick(e) {
+      this.markerLat = e.latlng.lat
+      this.markerLng = e.latlng.lng
+    }
+  }
+}
+</script>
+<style>
+.map-location {
+    width: 1000px;
+}
+</style>
