0dc47f1cbb477ac15093ef9d1380b2e72d426273
pedro <pedbcrespo@gmail.com>
2025-06-24 15:07:56 -0300

    implementando enum de tipos de demandas

--- src/enums/DemandType.js
+++ src/enums/DemandType.js
@@ -1,7 +1,9 @@
 export const DemandType = {
-    ENVIROMENT: {key: 1, name: 'Ambiental'},
-    STRUCTURAL: {key: 2, name: 'Estrutural'},
-    EMERGENCY: {key: 3, name: 'Emergencial'},
-    ENERGY: {key: 4, name: 'Energia Eletrica'},
-    WATER: {key: 5, name: 'Agua'},
+    ENVIROMENT: {key: 1, name: 'Ambiental', value: 'ENVIROMENT'},
+    STRUCTURAL: {key: 2, name: 'Estrutural', value: 'STRUCTURAL'},
+    EMERGENCY: {key: 3, name: 'Emergencial', value: 'EMERGENCY'},
+    ENERGY: {key: 4, name: 'Energia Eletrica', value: 'ENERGY'},
+    WATER: {key: 5, name: 'Agua', value: 'WATER'},
+    SANITATION: {key: 6, name: 'Saneamento', value: 'SANITATION'},
+    SUPERFICIAL: {key: 6, name: 'Superficial', value: 'SUPERFICIAL'},
 }
